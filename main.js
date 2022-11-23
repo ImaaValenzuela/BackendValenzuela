41e4f413ad321a7de168f77e9590b277feb2c1b2
enltd imaa <imanolvalenzuela03@gmail.com>
2022-11-22 22:52:58 -0300

    Programacion Backend - Clases con ECMAScript y ECMAScript avanzado

--- main.js
+++ main.js
@@ -1,94 +1,61 @@
-/* Definir clase TicketManager, el cual tendrá un arreglo de eventos que iniciará vacío
-La clase debe contar con una variable privada “precioBaseDeGanancia”, la cual añadirá un costo adicional al precio de cada evento.
-Debe contar con el método “getEventos” El cual mostrará los eventos guardados.
-Debe contar con el método “agregarEvento” El cual recibirá los siguientes parámetros:
-nombre
-lugar
-precio (deberá agregarse un 0.15 del valor original)
-capacidad (50 por defecto)
-fecha (hoy por defecto)
-El método deberá crear además el campo id autoincrementable y el campo “participantes” que siempre iniciará con un arreglo vacío.
-Debe contar con un método “agregarUsuario” El cual recibirá:
-id del evento (debe existir, agregar validaciones)
-id del usuario
-El método debe evaluar que el evento exista y que el usuario no haya estado registrado previamente (validación de fecha y capacidad se evitará para no alargar el reto)
-Si todo está en orden, debe agregar el id del usuario en el arreglo “participantes” de ese evento.
-Debe contar con un método “ponerEventoEnGira” El cual recibirá:
-id del evento
-nueva localidad
-nueva fecha
-El método debe copiar el evento existente, con una nueva localidad, nueva fecha, nuevo id y sus participantes vacíos (Usar spread operator para el resto de las propiedades)
- */
-
-class TicketManager {
-
-    #precioBaseDeGanancia
-    constructor() {
-        this.events = []
-        this.#precioBaseDeGanancia = 0.15
+class ProductManager{
+    constructor(){
+        this.products = [];
     }
 
-    getEvents = () => {return this.events}
-    
-    getNextID = () => {
-        const count = this.events.length
-        if (count == 0) return 1
+    getProducts = () =>{return this.products}
 
-        const lastEvent = this.events[count-1]
-        const lastID = lastEvent.id
-        const nextID = lastID + 1
-
-        return nextID
-
+    getNextID = () =>{
+        const count = this.products.length;
+        return (count>0) ? this.products[count-1].id + 1 : 1;
     }
 
-    addEvent = (nombre, lugar, precio, capacidad, fecha) => {
-        const id = this.getNextID()
+    existProduct = (code) => { 
+        return this.products.some((prod) => prod.code ===code);
+    }
 
-        const event = {
-            id,
-            nombre,
-            lugar,
-            precioBase: precio,
-            precio: precio * (1 + this.#precioBaseDeGanancia),
-            capacidad: capacidad ?? 50000,
-            fecha: fecha ?? new Date().toLocaleDateString(),
-            participants: []
+    getProductbyId = (id) => {
+        return this.products.find((prod) => prod.id == id) ?? "No se ha encontrado registro sobre este pirata.";
+    }
+
+    addProduct = (title, description, price, thumbnail, code, stock) =>{
+        if(!this.existProduct(code)){
+            const product = {
+                id: this.getNextID(),
+                code: code ?? 0,
+                title: title ?? "",
+                description: description ?? "",
+                price: price ?? 0.0,
+                thumbnail: thumbnail ?? "",
+                stock: stock ?? 0
+            }
+            this.products.push(product);
+        }else{
+            console.error(`Error, recompensa de ${title} actualizada. Su codigo ${code} ya existe con su nuevo valor`);
         }
-
-        this.events.push(event)
     }
-
-    addParticipant = (eventID, userID) => {
-        const event = this.events.find(event => event.id == eventID)
-        if(event == undefined) return -1
-
-        if(!event.participants.includes(userID)) {
-            event.participants.push(userID)
-            return 1
-        }
-
-        return -1
-    }
-
-    ponerEventoEnGira = (eventID, lugarNew, fechaNew) => {
-        const event = this.events.find(event => event.id == eventID)
-        const { nombre, precioBase, capacidad } = event
-
-        this.addEvent(nombre, lugarNew, precioBase, capacidad, fechaNew)
-    }
-
 }
 
-const manager = new TicketManager()
-manager.addEvent("Bad Bunny", "Estadio Velez", 120, null, null)
-manager.addEvent("Duki", "Estadio Velez", 1000, null, null)
+const productsManager = new ProductManager();
+console.log(productsManager.getProducts());
+console.log("\n ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ \n");
 
-manager.addParticipant(1, "Zoe")
-manager.addParticipant(1, "Imanol")
-manager.addParticipant(2, "Federico")
-manager.addParticipant(2, "Imanol")
+productsManager.addProduct("Monkey D Luffy", "Sombrero de Paja", 100000000, "https://cdn.shopify.com/s/files/1/0413/4253/3797/files/luffy_100_millions_large.jpg?v=1596110876", 1, 1);
 
-manager.ponerEventoEnGira(1, "Miami", null)
 
-console.log(manager.events);
+console.log(productsManager.getProducts());
+
+productsManager.addProduct("Roronoa Zoro", "Cazador de Piratas", 60000000, "https://pm1.narvii.com/6827/788c6961b3a97e847cd8c4bdd50b3f94a2a86d0dv2_hq.jpg", 2, 1);
+productsManager.addProduct("Monkey D Luffy", "Sombrero de Paja", 30000000, "http://pm1.narvii.com/6827/d6c22ef9af032e2b0e02de7f9c801872ef3ce00av2_00.jpg", 1, 0);
+
+console.log(productsManager.getProducts());
+console.log("\n ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ \n");
+
+console.log(productsManager.getProductbyId(1));
+console.log("\n ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ \n");
+
+console.log(productsManager.getProductbyId(2));
+console.log("\n ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ \n");
+
+console.log(productsManager.getProductbyId(5));
+console.log("\n ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ ⚓ \n");
