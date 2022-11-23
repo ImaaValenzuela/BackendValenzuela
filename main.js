e2254b35a7805f7b8c6135c013641a071ee8ebdc
enltd imaa <imanolvalenzuela03@gmail.com>
2022-11-22 22:05:58 -0300

    Programacion Backend - Nuevas funcionalidades de los lenguajes ECMAScritpt

--- main.js
+++ main.js
@@ -1,53 +1,94 @@
-/* Realizaremos una función que corrobore elementos en una lista. */
+/* Definir clase TicketManager, el cual tendrá un arreglo de eventos que iniciará vacío
+La clase debe contar con una variable privada “precioBaseDeGanancia”, la cual añadirá un costo adicional al precio de cada evento.
+Debe contar con el método “getEventos” El cual mostrará los eventos guardados.
+Debe contar con el método “agregarEvento” El cual recibirá los siguientes parámetros:
+nombre
+lugar
+precio (deberá agregarse un 0.15 del valor original)
+capacidad (50 por defecto)
+fecha (hoy por defecto)
+El método deberá crear además el campo id autoincrementable y el campo “participantes” que siempre iniciará con un arreglo vacío.
+Debe contar con un método “agregarUsuario” El cual recibirá:
+id del evento (debe existir, agregar validaciones)
+id del usuario
+El método debe evaluar que el evento exista y que el usuario no haya estado registrado previamente (validación de fecha y capacidad se evitará para no alargar el reto)
+Si todo está en orden, debe agregar el id del usuario en el arreglo “participantes” de ese evento.
+Debe contar con un método “ponerEventoEnGira” El cual recibirá:
+id del evento
+nueva localidad
+nueva fecha
+El método debe copiar el evento existente, con una nueva localidad, nueva fecha, nuevo id y sus participantes vacíos (Usar spread operator para el resto de las propiedades)
+ */
 
-const mostrarLista = array =>{
-    if(array.length == 0) return "La lista esta vacía."
+class TicketManager {
 
-    array.forEach(element =>{
-            console.log(element);
-    })
+    #precioBaseDeGanancia
+    constructor() {
+        this.events = []
+        this.#precioBaseDeGanancia = 0.15
+    }
 
-    return `La longitud del array es ${array.length}`
+    getEvents = () => {return this.events}
+    
+    getNextID = () => {
+        const count = this.events.length
+        if (count == 0) return 1
+
+        const lastEvent = this.events[count-1]
+        const lastID = lastEvent.id
+        const nextID = lastID + 1
+
+        return nextID
+
+    }
+
+    addEvent = (nombre, lugar, precio, capacidad, fecha) => {
+        const id = this.getNextID()
+
+        const event = {
+            id,
+            nombre,
+            lugar,
+            precioBase: precio,
+            precio: precio * (1 + this.#precioBaseDeGanancia),
+            capacidad: capacidad ?? 50000,
+            fecha: fecha ?? new Date().toLocaleDateString(),
+            participants: []
+        }
+
+        this.events.push(event)
+    }
+
+    addParticipant = (eventID, userID) => {
+        const event = this.events.find(event => event.id == eventID)
+        if(event == undefined) return -1
+
+        if(!event.participants.includes(userID)) {
+            event.participants.push(userID)
+            return 1
+        }
+
+        return -1
+    }
+
+    ponerEventoEnGira = (eventID, lugarNew, fechaNew) => {
+        const event = this.events.find(event => event.id == eventID)
+        const { nombre, precioBase, capacidad } = event
+
+        this.addEvent(nombre, lugarNew, precioBase, capacidad, fechaNew)
+    }
+
 }
 
-console.log(mostrarLista([1,2,3]));
-console.log(mostrarLista([]));
-console.log(mostrarLista(["Imanol", "Zoe"]));
+const manager = new TicketManager()
+manager.addEvent("Bad Bunny", "Estadio Velez", 120, null, null)
+manager.addEvent("Duki", "Estadio Velez", 1000, null, null)
 
-/* Se creará una clase que permitirá llevar cuentas individuales según cada responsable. */
+manager.addParticipant(1, "Zoe")
+manager.addParticipant(1, "Imanol")
+manager.addParticipant(2, "Federico")
+manager.addParticipant(2, "Imanol")
 
-class Contador{
+manager.ponerEventoEnGira(1, "Miami", null)
 
-    constructor(responsable){
-        this.responsable = responsable
-        this.cuentaLocal = 0
-    }
-
-    static cuentaGlobal = 0
-
-
-    getResponsable = () =>{
-        return this.responsable
-    }
-
-    getCuentaIndividual = () => {return this.cuentaLocal}
-
-    getCuentaGlobal = () => { return Contador.cuentaGlobal}
-
-    contar(){
-        this.cuentaLocal++
-        Contador.cuentaGlobal++
-    }
-}
-
-const persona1 = new Contador("P1")
-const persona2 = new Contador ("P2")
-const persona3 = new Contador("P3")
-
-persona1.contar()
-persona1.contar()
-persona2.contar()
-
-console.log(`${persona1.getResponsable()} : ${persona1.getCuentaIndividual()}`)
-console.log(`${persona2.getResponsable()} : ${persona2.getCuentaIndividual()}`)
-console.log(Contador.cuentaGlobal)
+console.log(manager.events);
